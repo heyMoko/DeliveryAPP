7f3488d90b24014cb8ee1e8054787f4ff04dcb6e
heymoko <gozila__adm@naver.com>
2022-05-26 15:14:59 +0900

    clean architecture set

--- app/src/main/java/com/project/deliveryapp/screen/main/MainActivity.kt
+++ app/src/main/java/com/project/deliveryapp/screen/main/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.project.deliveryapp
+package com.project.deliveryapp.screen.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.project.deliveryapp.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
